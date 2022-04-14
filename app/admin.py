751ffe9fa45f346ab24cb2cb1ddaaf9d2e8f5142
ROHITNANERA <rohit_nanera@yahoo.com>
2022-04-14 11:31:57 +0530

    first commit

---
+++ app/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from .models import *
+# Register your models here.
+
+class HAdminAdmin(admin.ModelAdmin):
+    list_display = ('id','name','email','Password')
+admin.site.register(HAdmin,HAdminAdmin)
+
+class HostelAdmin(admin.ModelAdmin):
+    list_display = ('id','h_name','h_user','h_email','h_contact','h_city','h_address','h_fees','h_registered')
+admin.site.register(Hostel,HostelAdmin)
+
+class RoomAdmin(admin.ModelAdmin):
+    list_display=('id','room_no','hostel')
+admin.site.register(Room,RoomAdmin)
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('id','name','email','contact','hostel','room')
+admin.site.register(Student,StudentAdmin)
+
+class FacilityAdmin(admin.ModelAdmin):
+    list_display = ('id','hostel','f_name','f_description')
+admin.site.register(Facility,FacilityAdmin)
+
+class ImageAdmin(admin.ModelAdmin):
+    list_display=('id','hostel','file')
+admin.site.register(Image,ImageAdmin)
---
+++ app/models.py
@@ -0,0 +1,61 @@
+from tokenize import Triple
+from django.db import models
+from django.contrib.auth.models import User
+from django.forms import PasswordInput
+
+
+
+
+class HAdmin(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    Password = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+
+
+
+class Hostel(models.Model):
+    h_name = models.CharField(max_length=100,verbose_name="Hostel Name")
+    h_user = models.ForeignKey(HAdmin,verbose_name="Admin",on_delete=models.SET_NULL,null=True)         #hostel admin
+    h_email = models.EmailField(max_length=100,verbose_name="Hostel Email")
+    h_contact = models.CharField(max_length=100,verbose_name="Hostel Contact")
+    h_city = models.CharField(max_length=30,verbose_name="City",null=True)
+    h_address = models.TextField(max_length=100,verbose_name="Hostel Address")
+    h_fees = models.IntegerField(null=True,verbose_name="Hostel Fees")
+    h_registered = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.h_name
+
+class Facility(models.Model):
+    hostel = models.ForeignKey(Hostel,on_delete=models.CASCADE,verbose_name="Hostel")
+    f_name = models.CharField(max_length=100,null=True,verbose_name="Hostel Facilities")
+    f_description = models.CharField(max_length=200)
+
+class Image(models.Model):
+    hostel = models.ForeignKey(Hostel,on_delete=models.CASCADE)
+    file = models.CharField(max_length=200)
+    
+
+#create room model
+# roomid,room number,hostelid
+class Room(models.Model):
+    room_no = models.CharField(max_length=30)
+    hostel = models.ForeignKey(Hostel,on_delete=models.SET_NULL,null=True)
+    capacity = models.CharField(max_length=30,null=True)
+
+    def __str__(self):
+        # return str(self.hostel)+str(self.room_no)
+        return str(self.room_no)
+
+
+
+#create student model
+# name,email,contact,address,hostelid,roomid
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    contact = models.CharField(max_length=30)
+    hostel = models.ForeignKey(Hostel,on_delete=models.SET_NULL,null=True)
+    room = models.ForeignKey(Room,on_delete=models.SET_NULL,null = True)
